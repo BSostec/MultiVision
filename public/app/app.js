56c489481359956eed17bca2f60ed036f9b8c1ac
Bartolo Carnemolla <Bartolo Carnemolla>
2016-09-16 16:24:10 +0200

    Added data courses and some UI

--- public/app/app.js
+++ public/app/app.js
@@ -9,10 +9,6 @@ angular.module('app', [
 angular.module('app').config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl' });
+        .when('/', { templateUrl: '/partials/main', controller: 'mvMainCtrl' });
 
-});
-
-angular.module('app').controller('mainCtrl', function ($scope) {
-    $scope.myVar = "Hello Angular";
 });
