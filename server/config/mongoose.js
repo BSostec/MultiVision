be9b58e21e54942977ad90428a8d2cbe721c86c2
Bartolo Carnemolla <Bartolo Carnemolla>
2016-09-17 10:54:49 +0200

    Added hashing password methods

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,8 @@
  * Created by Bartolo on 16/09/2016.
  */
 
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    crypto = require('crypto');
 
 module.exports = function (config) {
     mongoose.connect(config.db);
@@ -15,18 +16,47 @@ module.exports = function (config) {
     var userSchema = mongoose.Schema({
         firstName: String,
         lastName: String,
-        username: String
+        username: String,
+        salt: String,
+        hashed_pwd: String
     });
+    userSchema.methods = {
+        authenticate: function (passwordToMatch) {
+            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+        }
+    };
     var User = mongoose.model('User', userSchema);
 
     User.find({}).exec(function (err, collection) {
         if(collection.length === 0) {
-            User.create({firstName:'Bartolo', lastName:'Carnemolla', username:'bartolo'});
-            User.create({firstName:'Elio', lastName:'Puglisi', username:'elio'});
-            User.create({firstName:'Andrea', lastName:'Todaro', username:'andrea'});
-            User.create({firstName:'Pinella', lastName:'Adamo', username:'pinella'});
-            User.create({firstName:'Gianni', lastName:'Carnemolla', username:'gianni'});
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'sostec');
+            User.create({firstName:'Bartolo', lastName:'Carnemolla', username:'bartolo', salt: salt, hashed_pwd: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'sostec');
+            User.create({firstName:'Elio', lastName:'Puglisi', username:'elio', salt: salt, hashed_pwd: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'sostec');
+            User.create({firstName:'Andrea', lastName:'Todaro', username:'andrea', salt: salt, hashed_pwd: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'sostec');
+            User.create({firstName:'Pinella', lastName:'Adamo', username:'pinella', salt: salt, hashed_pwd: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'sostec');
+            User.create({firstName:'Gianni', lastName:'Carnemolla', username:'gianni', salt: salt, hashed_pwd: hash});
         }
     })
-
 };
+
+function createSalt() {
+    return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+    var hmac = crypto.createHmac('sha1', salt);
+    hmac.setEncoding('hex');
+    hmac.write(pwd);
+    hmac.end();
+    return hmac.read();
+}
