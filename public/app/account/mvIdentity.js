e32f5b087ff998fd7fad4fde67b95ebe6770e7eb
Bartolo Carnemolla <Bartolo Carnemolla>
2016-09-17 11:45:10 +0200

    Before Authorization...

--- public/app/account/mvIdentity.js
+++ public/app/account/mvIdentity.js
@@ -1,8 +1,11 @@
 /**
  * Created by Bartolo on 17/09/2016.
  */
-angular.module('app').factory('mvIdentity', function () {
+angular.module('app').factory('mvIdentity', function ($window) {
     var currentUser;
+    if(!!$window.bootstrappedUserObject) {
+        currentUser = $window.bootstrappedUserObject;
+    }
     return {
         currentUser: currentUser,
         isAuthenticated: function () {
--- server/config/routes.js
+++ server/config/routes.js
@@ -17,6 +17,8 @@ module.exports = function (app) {
     });
 
     app.get('*', function (req, res) {
-        res.render('index');
+        res.render('index', {
+            bootstrappedUser: req.user
+        });
     });
 };
