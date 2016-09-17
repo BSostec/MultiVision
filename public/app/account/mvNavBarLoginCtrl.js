71c3f0e84b8ab6ad51b9e3f59d8f848ef41a9129
Bartolo Carnemolla <Bartolo Carnemolla>
2016-09-17 11:19:03 +0200

    Added sign out functionality

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by Bartolo on 16/09/2016.
  */
-angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
     $scope.identity = mvIdentity;
     $scope.signin = function (username, password) {
         mvAuth.authenticateUser(username, password).then(function (success) {
@@ -11,5 +11,15 @@ angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, m
                 mvNotifier.notify('Username/Password combination incorrect');
             }
         });
+    }
+    
+    $scope.signout = function () {
+        mvAuth.logoutUser().then(function () {
+            $scope.username = "";
+            $scope.password = "";
+            mvNotifier.notify('You have successfully signed out!');
+            $location.path('/');
+        })
+        
     }
 });
