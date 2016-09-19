d85e0fd212c390aa9c0cf645ba0a7ce674c06a38
Bartolo Carnemolla <Bartolo Carnemolla>
2016-09-19 16:02:26 +0200

    Added new products in homepage

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,6 +1,7 @@
 /**
  * Created by Bartolo on 16/09/2016.
  */
-angular.module('app').controller('mvMainCtrl', function ($scope, mvCachedCourses) {
+angular.module('app').controller('mvMainCtrl', function ($scope, mvCachedCourses, mvCachedProducts) {
     $scope.courses = mvCachedCourses.query();
+    $scope.products = mvCachedProducts.query();
 });
